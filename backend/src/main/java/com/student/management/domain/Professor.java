2fd7a6b4bd53499a1e7a25f4fb6e79c2fa7681f8
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-05-16 20:06:28 +0100

    add payment status enum and subject model; refactor app module declarations

--- backend/src/main/java/com/student/management/domain/Professor.java
+++ backend/src/main/java/com/student/management/domain/Professor.java
@@ -103,7 +103,6 @@ public class Professor implements Serializable {
 
     @Override
     public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
         return getClass().hashCode();
     }
 
