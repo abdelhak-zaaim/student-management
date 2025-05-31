06cf2af65d78f60fdc85b0c3da45f230aa948034
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-05-31 12:02:58 +0100

    Refactor admin routing: remove redundant routes and streamline navigation structure

--- frontend/src/app/components/admin/admin-routing.module.ts
+++ frontend/src/app/components/admin/admin-routing.module.ts
@@ -5,19 +5,6 @@ import { UpdateComponent } from './update/update.component';
 import { AdminComponent } from './admin.component';
 import {AddComponent} from "../professor/add/add.component";
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AdminComponent,
-    children: [
-      { path: '', component: ListComponent },
-      { path: 'list', component: ListComponent },
-      { path: 'add', component: UpdateComponent },
-      { path: 'edit/:id', component: UpdateComponent },
-      { path: '', redirectTo: 'list', pathMatch: 'full' }
-    ]
-  }
-];
 
 @NgModule({
     imports: [RouterModule.forChild([
